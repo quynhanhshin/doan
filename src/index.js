767843838c0dc7e3e3f055c8fb3ceb1444ed37ad
quynhanhshin <shinanh21@gmail.com>
2024-01-12 13:17:09 +0700

    BTVN add

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-
+{
+    /* <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>; */
+}
+// href = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-        <React.StrictMode>
-                <App />
-        </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
