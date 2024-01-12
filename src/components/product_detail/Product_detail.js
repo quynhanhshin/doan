5bdca1acd2e3cf70f04405e0ea9bafeebecffb26
quynhanhshin <shinanh21@gmail.com>
2024-01-12 12:49:55 +0700

    BTVN Hoan chinh product

--- src/components/product_detail/Product_detail.js
+++ src/components/product_detail/Product_detail.js
@@ -4,24 +4,27 @@ import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Col } from "
 import product_detail from "./product_detail.css";
 
 export default function Product_detail(props) {
-        const { product } = props;
-        return (
-                <Col sm={6} md={4} lg={3}>
-                        <div className="product-detail">
-                                <Link to={`/product-detail/${product.id}`}>
-                                        <Card>
-                                                <img alt="Sample" src="https://picsum.photos/300/200" />
-                                                <CardBody>
-                                                        <CardSubtitle className="mb-2 text-muted" tag="h6">
-                                                                {product.type}
-                                                        </CardSubtitle>
-                                                        <CardTitle tag="h5">{product.name}</CardTitle>
-                                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card‘s content.</CardText>
-                                                        <Button>Button</Button>
-                                                </CardBody>
-                                        </Card>
-                                </Link>
-                        </div>
-                </Col>
-        );
+    const { product } = props;
+    return (
+        <Col sm={6} md={4} lg={4}>
+            <div className="product-detail">
+                <Link to={`/product-detail/${product.id}`}>
+                    <Card>
+                        <img alt="Sample" src="https://picsum.photos/300/200" />
+                        <CardBody>
+                            <CardSubtitle className="mb-2 text-type" tag="h6">
+                                {product.type}
+                            </CardSubtitle>
+                            <CardTitle tag="h5">{product.name}</CardTitle>
+                            <CardSubtitle className="mb-2 text-price" tag="h6">
+                                ${product.price}
+                            </CardSubtitle>
+                            {/* <CardText>Some quick example text to build on the card title and make up the bulk of the card‘s content.</CardText> */}
+                            {/* <Button>Button</Button> */}
+                        </CardBody>
+                    </Card>
+                </Link>
+            </div>
+        </Col>
+    );
 }
