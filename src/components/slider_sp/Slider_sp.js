ac4dfa72392b7de0da54f8ca663282ca7fa1332f
quynhanshin <shinanh21@gmail.com>
2024-02-19 21:06:00 +0700

    sliderimg

--- src/components/slider_sp/Slider_sp.js
+++ src/components/slider_sp/Slider_sp.js
@@ -1,43 +1,84 @@
 import React from "react";
 import Slider from "react-slick";
 import "./slider_sp.css";
-
-export default function Sli() {
+import pic01 from "../../imgs/slider-img1.png";
+import pic02 from "../../imgs/slider-img2.png";
+import pic03 from "../../imgs/slider-img3.png";
+import pic04 from "../../imgs/slider-img4.png";
+import pic05 from "../../imgs/slider-img5.png";
+import pic06 from "../../imgs/slider-img6.png";
+import pic07 from "../../imgs/slider-img7.png";
+export default function Responsive() {
         const settings = {
-                // dots: true,
                 infinite: true,
                 speed: 500,
-                slidesToShow: 4,
+                slidesToShow: 8,
                 slidesToScroll: 1,
+                responsive: [
+                        {
+                                breakpoint: 1024,
+                                settings: {
+                                        slidesToShow: 3,
+                                        slidesToScroll: 3,
+                                        infinite: true,
+                                        dots: true,
+                                },
+                        },
+                        {
+                                breakpoint: 600,
+                                settings: {
+                                        slidesToShow: 2,
+                                        slidesToScroll: 2,
+                                        initialSlide: 2,
+                                },
+                        },
+                        {
+                                breakpoint: 480,
+                                settings: {
+                                        slidesToShow: 1,
+                                        slidesToScroll: 1,
+                                },
+                        },
+                ],
         };
         return (
-                <div>
-                        <Slider {...settings}>
-                                <div>
-                                        <h3>1</h3>
-                                </div>
-                                <div>
-                                        <h3>2</h3>
-                                </div>
-                                <div>
-                                        <h3>3</h3>
-                                </div>
-                                <div>
-                                        <h3>4</h3>
-                                </div>
-                                <div>
-                                        <h3>5</h3>
-                                </div>
-                                <div>
-                                        <h3>6</h3>
-                                </div>
-                                <div>
-                                        <h3>7</h3>
-                                </div>
-                                <div>
-                                        <h3>8</h3>
-                                </div>
-                        </Slider>
+                <div className="meo">
+                        <div className="slider2">
+                                <Slider {...settings}>
+                                        <div>
+                                                <img src={pic01} />
+                                                <p>Other</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic02} />
+                                                <p>Specials</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic03} />
+                                                <p>Carriage</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic01} />
+                                                <p>New</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic05} />
+                                                <p>Bottles</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic06} />
+                                                <p>Cribs</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic07} />
+                                                <p>Beds</p>
+                                        </div>
+                                        <div>
+                                                <img src={pic01} />
+                                                <p>Toys</p>
+                                        </div>
+                                </Slider>
+                        </div>
                 </div>
         );
 }
